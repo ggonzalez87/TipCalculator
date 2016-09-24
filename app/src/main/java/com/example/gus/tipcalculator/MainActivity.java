395aa7a4660ce13204360db2b2f0da36c90a6461
ggonzalez87 <buraiden917@gmail.com>
2016-09-24 04:59:51 -0700

    Added Splash Screen, Changed App Icon Color
    Added a splash screen with slight delay, changed app icon colors to
    reflect app theme.

--- app/src/main/java/com/example/gus/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/gus/tipcalculator/MainActivity.java
@@ -3,21 +3,43 @@ package com.example.gus.tipcalculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
 
     private RadioGroup rg1;
+    private String result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Delay for splash screen only
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //radio buttons
         rg1 = (RadioGroup) findViewById(R.id.radio);
         rg1.setOnCheckedChangeListener(this);
+
+        //calculate button
+        final Button button = (Button) findViewById(R.id.calculate_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                result = "WORKS";
+                setTextView((TextView) findViewById(R.id.result), result);
+            }
+        });
     }
 
     private void disableEditText(EditText editText){
@@ -28,6 +50,10 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
     private void enableEditText(EditText editText){
         //editText.setFocusable(true);
         editText.setEnabled(true);
+    }
+
+    private void setTextView(TextView textView, String string){
+        textView.setText(result);
     }
 
     @Override
