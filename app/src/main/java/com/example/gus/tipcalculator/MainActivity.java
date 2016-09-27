765035c333695ea3050f9aa4f6ebea9000a034f8
ggonzalez87 <buraiden917@gmail.com>
2016-09-26 20:36:10 -0700

    Completion of app
    Added TextView for tip amount and total bill, filtered TextView to
    simulate two decimal places for currency. Added scrollview when phone is
    tilted to side.

--- app/src/main/java/com/example/gus/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/gus/tipcalculator/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.text.InputFilter;
 import android.view.View;
 import android.widget.Button;
@@ -18,14 +19,15 @@ import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity implements RadioGroup.OnCheckedChangeListener {
 
     private RadioGroup rg1;
-    private String result;
+    private String tipResult, total;
     private EditText billInputText;
     private EditText customTipInputText;
-    private Double billInput;
-    private Double customTipInput;
+    private Double billInput, customTipInput;
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -60,6 +62,7 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
                 calculateInput();
             }
         });
+
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
@@ -76,17 +79,17 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
     }
 
     private void setTextView(TextView textView, String string) {
-        textView.setText(result);
+        textView.setText(tipResult);
     }
 
     @Override
     public void onCheckedChanged(RadioGroup group, int checkedId) {
         //Disables custom tip input if custom tip radio button not selected
         switch (checkedId) {
+
             case R.id.custom_percent_button:
                 enableEditText((EditText) findViewById(R.id.custom_tip_input));
                 break;
-
             case R.id.ten_percent_button:
                 disableEditText((EditText) findViewById(R.id.custom_tip_input));
                 break;
@@ -100,14 +103,18 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
     private void calculateInput() {
         billInputText = (EditText) findViewById(R.id.bill_input);
         customTipInputText = (EditText) findViewById(R.id.custom_tip_input);
+        RadioButton crb = (RadioButton) findViewById(R.id.custom_percent_button);
 
+        //Checks to see if there is valid input when from radio button selected
         try {
 
             billInput = Double.parseDouble(billInputText.getText().toString());
-            customTipInput = Double.parseDouble(customTipInputText.getText().toString());
 
-            result = "WORKS";
-            setTextView((TextView) findViewById(R.id.result), result);
+            if (crb.isChecked()) {
+                customTipInput = Double.parseDouble(customTipInputText.getText().toString());
+            }
+
+            calculateResult(billInput, customTipInput);
 
         } catch (NumberFormatException e) {
 
@@ -119,18 +126,38 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
             toast.show();
         }
 
-
-        System.out.println("Bill Input: " + billInput);
-        System.out.println("Tip Input: " + customTipInput);
     }
 
-    private boolean validateInput() {
-        billInputText = (EditText) findViewById(R.id.bill_input);
-        customTipInputText = (EditText) findViewById(R.id.custom_tip_input);
+    private void calculateResult(Double billInput, Double customTipInput) {
+        //Implement results here
+        //Find textView, set text using Html.fromHtml(result);
+        Double tipCalc;
+        int checkedId = rg1.getCheckedRadioButtonId();
+        TextView calculation = (TextView) findViewById(R.id.result);
+        TextView totalView = (TextView) findViewById(R.id.total_text_view);
 
-        billInput = Double.parseDouble(billInputText.getText().toString());
-        customTipInput = Double.parseDouble(customTipInputText.getText().toString());
+        DecimalFormat twoDForm = new DecimalFormat("#.00");
 
-        return false;
+        switch (checkedId) {
+            case R.id.custom_percent_button:
+                tipCalc = Math.round(billInput * (customTipInput / 100.0) * 100.0) / 100.0;
+                tipResult = "Tip amount is <b>" + twoDForm.format(tipCalc) + "</b>";
+                total = "New total bill will be <b>" + twoDForm.format(tipCalc + billInput) + "</b>";
+                break;
+            case R.id.ten_percent_button:
+                tipCalc = Math.round((billInput * .10) * 100.0) / 100.0;
+                tipResult = "Tip amount is <b>" + twoDForm.format(tipCalc) + "</b>";
+                total = "New total bill will be <b>" + twoDForm.format(tipCalc + billInput) + "</b>";
+                break;
+            case R.id.twenty_percent_button:
+                tipCalc = Math.round((billInput * .20) * 100.0) / 100.0;
+                tipResult = "Tip amount is <b>" + twoDForm.format(tipCalc) + "</b>";
+                total = "New total bill will be <b>" + twoDForm.format(tipCalc + billInput) + "</b>";
+                break;
+        }
+
+        calculation.setText(Html.fromHtml(tipResult));
+        totalView.setText(Html.fromHtml(total));
+
     }
 }
